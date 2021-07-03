5f29a92f924ee1d24baf00c9706de6d905b5cf5a
1000103918 <alok072001@gmail.com>
2021-07-03 16:07:22 +0530

    completed

--- me.js
+++ me.js
@@ -1,33 +1,61 @@
-
-const random = Math.trunc(Math.random() * 20);
+let random = Math.trunc(Math.random() * 20);
 let score = 20;
+let highScore = 0;
+
+function message(message) {
+    document.querySelector('.message').textContent = message;
+}
+
+// Again
+document.querySelector('.head_button').addEventListener('click', function () {
+    score = 20;
+    random = Math.trunc(Math.random() * 20);
+    document.querySelector('.play_in').value = '';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.final_n').textContent = '?';
+    message('Start guessing...');
+    document.querySelector('body').style.backgroundColor = 'black';
+    document.querySelector('.message').style.color = 'white';
+})
+
+// PLay
 document.querySelector('.play_btn').addEventListener('click', function () {
     let guess = Number(document.querySelector('.play_in').value);
 
     if (!guess) {
-        document.querySelector('.message').textContent = 'No number  ❓';
+        message('No number  ❓');
+    }
+
+
+    else if (guess !== random) {
+        if (score > 1) {
+
+            message(guess < random ? 'Too low  📉' : 'Too high  📈');
+            score--;
+            document.querySelector('.score').textContent = score;
+
+
+        }
     }
     else if (guess === random) {
-        document.querySelector('.message').textContent = 'You Win🥇';
+        message('You Win🥇');
         document.querySelector('.final_n').textContent = guess;
+        document.querySelector('body').style.backgroundColor = 'green';
+        document.querySelector('.final_n').style.paddingRight = '65px';
+        document.querySelector('.final_n').style.paddingLeft = '55px';
 
-    }
-    else if (guess !== random) {
-        if (guess < random) {
-            document.querySelector('.message').textContent = 'Too low  📉';
-            score--;
-            document.querySelector('.score').textContent = score;
-        }
-
-
-        else if (guess > random) {
-            document.querySelector('.message').textContent = 'Too high  📈';
-            score--;
-            document.querySelector('.score').textContent = score;
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
         }
 
     }
 
+    else {
+        message('You loose! TRY AGAIN🔴');
+        document.querySelector('.score').textContent = '0'
+        document.querySelector('.message').style.color = 'red';
+    }
 })
 
 
